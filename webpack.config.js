9acbf02f9c3e9eedca7423836ff3a1612c36e83a
kinsk2839 <kinsk2839@naver.com>
2023-11-18 00:22:01 +0900

    chore: webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: process.env.MODE,
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "assets/[contenthash].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new webpack.DefinePlugin({
+      mode: process.env.MODE,
+      port: process.env.PORT,
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: process.env.PORT,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
