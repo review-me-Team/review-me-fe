216d2dd627991cae6c1f5ee47f3b4094cec116c1
kinsk2839 <kinsk2839@naver.com>
2023-11-21 00:35:12 +0900

    chore: 절대 경로 설정

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,17 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+    },
   },
   module: {
     rules: [
@@ -35,7 +45,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: "./public/index.html",
     }),
     new webpack.DefinePlugin({
       mode: process.env.MODE,
