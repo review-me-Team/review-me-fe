6db482452e3186f7431bea7e17d24484dc948a9a
kinsk2839 <kinsk2839@naver.com>
2023-11-22 13:20:43 +0900

    chore: tanstack query 설치
    - 우선 설정만 해놓고 나중에 마이그레이션 할 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const App = () => (
-  <>
-    <h1>React!</h1>
-  </>
-);
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <h1>React!</h1>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
