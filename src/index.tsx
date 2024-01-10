c8a52fb51f56d2272f1d560f77b1e39676ca8fe1
aken-you <kinsk2839@naver.com>
2024-01-11 00:07:38 +0900

    feat: GlobalStyle 생성 (#15)
    - body의 높이를 브라우저 화면의 높이로 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { ReviewMeProvider } from 'review-me-design-system';
+import { GlobalStyle } from '@styles/GlobalStyle';
 import router from './router';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ReviewMeProvider>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </ReviewMeProvider>
   </React.StrictMode>,
