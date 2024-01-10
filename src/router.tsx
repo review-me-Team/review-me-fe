df09526cad16bbc989840c7ecb5e116f6606d479
aken-you <kinsk2839@naver.com>
2024-01-10 19:16:33 +0900

    feat: 내 이력서 페이지 path 설정 (#15)

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import MainPage from '@pages/MainPage';
 import MyPage from '@pages/MyPage';
+import MyResume from '@pages/MyResume';
 import Resume from '@pages/Resume';
 import ResumeDetail from '@pages/ResumeDetail';
 import { ROUTE_PATH } from '@constants';
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
   { path: ROUTE_PATH.MY_PAGE, element: <MyPage /> },
   { path: ROUTE_PATH.RESUME, element: <Resume /> },
   { path: `${ROUTE_PATH.RESUME}/:id`, element: <ResumeDetail /> },
+  { path: ROUTE_PATH.MY_RESUME, element: <MyResume /> },
 ]);
 
 export default router;
