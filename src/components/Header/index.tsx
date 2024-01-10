567bd5402a43f5ce595384fd0af8adf0fc0a7711
aken-you <kinsk2839@naver.com>
2024-01-10 16:25:47 +0900

    feat: Header 컴포넌트 UI 생성 (#13)

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Button, Icon, theme } from 'review-me-design-system';
+import {
+  HeaderLayout,
+  LeftContainer,
+  MenuItem,
+  MenuList,
+  NavContainer,
+  ReviewMe,
+  RightContainer,
+  UserInfoButton,
+} from './style';
+
+const Header = () => {
+  return (
+    <HeaderLayout>
+      <NavContainer>
+        <RightContainer>
+          <ReviewMe>review me</ReviewMe>
+          <MenuList>
+            <MenuItem>이력서</MenuItem>
+            <MenuItem>자소서</MenuItem>
+          </MenuList>
+        </RightContainer>
+
+        <LeftContainer>
+          <UserInfoButton>
+            <Icon iconName="person" color={theme.color.accent.text.strong} width={32} height={32} />
+          </UserInfoButton>
+          <Button variant="default" size="s">
+            로그아웃
+          </Button>
+        </LeftContainer>
+      </NavContainer>
+    </HeaderLayout>
+  );
+};
+
+export default Header;
