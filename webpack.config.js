6ea4c4e54e91ec27f1e208bdef4d4f33b631303b
aken-you <kinsk2839@naver.com>
2024-01-22 21:33:57 +0900

    chore: webpack css 관련 loader 설정 (#18)

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.svg$/,
