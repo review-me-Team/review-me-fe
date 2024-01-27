fd61a97810c34a854de288877780b24444ca2983
kinsk2839 <96980857+aken-you@users.noreply.github.com>
2024-01-27 15:24:08 +0900

    [FE] 이력서 메인 페이지 UI (#24)
    * feat: 글자 길이가 길어지면 말 줄임표로 표시하는 css 생성 (#23)
    
    * feat: ResumeItem 컴포넌트 생성 (#23)
    
    - Resume 목록의 item 컴포넌트
    
    * feat: ResumeItem storybook 작성 (#23)
    
    * feat: 이력서 메인 페이지 UI (#23)

---
+++ src/styles/common.ts
@@ -0,0 +1,8 @@
+import { css } from 'styled-components';
+
+export const ellipsisStyles = css`
+  overflow-x: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  word-break: break-all;
+`;
