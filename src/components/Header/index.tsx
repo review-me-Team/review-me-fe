71fb5d59e6cc437e1b464deca9dd60776848d4e9
aken-you <kinsk2839@naver.com>
2024-01-29 20:49:15 +0900

    refactor: MobileMenu에서 My 이력서 보러가기 menu 빼기 (#28)
    - 로그인 한 상태에서만 보이도록 하기 위해서 나중에 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -66,15 +66,6 @@ const Header = () => {
                   <span>이력서 보러가기</span>
                   <Icon iconName="rightArrow" width={28} height={28} />
                 </MobileMenuItem>
-                <MobileMenuItem
-                  onClick={() => {
-                    handleCloseMobileMenu();
-                    navigate(ROUTE_PATH.MY_RESUME);
-                  }}
-                >
-                  <span>My 이력서 보러가기</span>
-                  <Icon iconName="rightArrow" width={28} height={28} />
-                </MobileMenuItem>
               </MobileMenuList>
 
               <JoinUsMessage>
