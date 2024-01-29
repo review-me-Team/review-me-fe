f0ba6900c3a910cdbb12fbb6d82a723943329fab
aken-you <kinsk2839@naver.com>
2024-01-29 16:44:20 +0900

    feat: 사람 아이콘 버튼의 click handler 구현 (#28)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button, Icon, theme } from 'review-me-design-system';
 import useMediaQuery from '@hooks/useMediaQuery';
 import { ROUTE_PATH } from '@constants';
@@ -15,6 +15,7 @@ import {
 } from './style';
 
 const Header = () => {
+  const navigate = useNavigate();
   const { matches: isSMDevice } = useMediaQuery({ mediaQueryString: '(max-width: 600px)' });
 
   return (
@@ -38,7 +39,13 @@ const Header = () => {
         )}
 
         <LeftContainer>
-          <IconButton>
+          <IconButton
+            onClick={() => {
+              // * 로그인 된 상태일 경우 마이페이지로 이동
+              navigate(ROUTE_PATH.MY_PAGE);
+              // todo: 로그인하지 않았을 경우 로그인 유도하는 로직 구현하기
+            }}
+          >
             <Icon iconName="person" color={theme.color.accent.text.strong} width={32} height={32} />
           </IconButton>
           {!isSMDevice && (
