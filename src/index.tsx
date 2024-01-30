59e949eab83e1535e4a730ce0bfce34ac9f43e87
aken-you <kinsk2839@naver.com>
2024-01-30 16:34:34 +0900

    feat: queryClient 생성 및 적용 (#30)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReviewMeProvider } from 'review-me-design-system';
 import { GlobalStyle } from '@styles/GlobalStyle';
 import router from './router';
+
+const queryClient = new QueryClient();
 
 const main = async () => {
   if (process.env.NODE_ENV === 'development') {
@@ -14,10 +18,13 @@ const main = async () => {
 
   createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <ReviewMeProvider>
-        <GlobalStyle />
-        <RouterProvider router={router} />
-      </ReviewMeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ReviewMeProvider>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </ReviewMeProvider>
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 };
