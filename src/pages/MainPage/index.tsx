63a993d76c284a9502eb271e30290de245990f88
aken-you <kinsk2839@naver.com>
2024-02-25 12:24:16 +0900

    feat: MainPage에서 test용으로 get 요청 (#49)

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MainPage = () => {
+  // * test용
+  useEffect(() => {
+    fetch(`https://api.review-me.co.kr/test`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return <div>MainPage</div>;
 };
 
