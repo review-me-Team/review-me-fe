8fc33357cc90d21ca595f602bbd2515ddda6594b
aken-you <kinsk2839@naver.com>
2024-02-26 15:35:02 +0900

    fix: jwt가 user context에 저장되지 않는 문제 발생 (#61)

--- src/apis/login.ts
+++ src/apis/login.ts
@@ -1,4 +1,6 @@
+import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
+import { useUserContext } from '@contexts/userContext';
 import { REQUEST_URL } from '@constants';
 import { ApiResponse } from './response.types';
 
@@ -26,7 +28,14 @@ const postAuthorizationCode = async (code: string) => {
 };
 
 export const usePostAuthorizationCode = () => {
+  const navigate = useNavigate();
+  const { login } = useUserContext();
+
   return useMutation({
     mutationFn: postAuthorizationCode,
+    onSuccess: ({ jwt }) => {
+      login(jwt);
+      navigate('/');
+    },
   });
 };
--- src/pages/SocialLogin/index.tsx
+++ src/pages/SocialLogin/index.tsx
@@ -1,22 +1,13 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useUserContext } from '@contexts/userContext';
 import { usePostAuthorizationCode } from '@apis/login';
 
 const SocialLogin = () => {
-  const navigate = useNavigate();
   const code = new URLSearchParams(window.location.search).get('code');
-  const { mutate, data } = usePostAuthorizationCode();
-  const { login } = useUserContext();
+  const { mutate } = usePostAuthorizationCode();
 
   useEffect(() => {
     if (code) {
       mutate(code);
-      // todo: 이전 페이지로 이동 (현재는 메인 페이지로 이동하도록 구현)
-      navigate('/');
-      if (data?.jwt) {
-        login(data.jwt);
-      }
     }
   }, []);
 
