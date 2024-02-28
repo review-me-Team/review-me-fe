1b250c9381a2d9d183a5f3866e69d6291c055a16
aken-you <kinsk2839@naver.com>
2024-02-28 23:45:08 +0900

    refactor: 이력서 업로드 페이지에 ResumeForm 컴포넌트 적용 (#67)

--- src/pages/ResumeUpload/style.ts
+++ src/pages/ResumeUpload/style.ts
@@ -25,31 +25,6 @@ const ResumeUploadContainer = styled.div`
   background-color: ${theme.color.neutral.bg.default};
 `;
 
-const ResumeUploadFormContainer = styled.div<{ $width?: string }>`
-  display: flex;
-  width: ${({ $width }) => $width || '100%'};
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    gap: 0.5rem;
-  }
-`;
-
-const ResumeUploadForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-end;
-  width: 45%;
-  margin-left: 1rem;
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    gap: 0.5rem;
-    margin-left: 0;
-  }
-`;
-
 const Description = styled.div`
   display: flex;
   flex-direction: column;
@@ -67,53 +42,4 @@ const SubDescription = styled.span`
   color: ${theme.color.neutral.text.strong}
 `;
 
-const FieldContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  width: 100%;
-`;
-
-const Field = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Label = styled.label`
-  display: flex;
-  align-items: center;
-  flex-shrink: 0;
-  width: 6.5rem;
-
-  color: ${theme.color.neutral.text.default};
-  ${theme.font.body.default}
-`;
-
-const FileLabel = styled.label`
-  display: flex;
-  padding: 0.5rem 0.75rem;
-  justify-content: center;
-  align-items: center;
-  margin-right: 0.5rem;
-
-  border-radius: 0.75rem;
-  border: 1px solid ${theme.color.accent.bd.strong};
-  background: ${theme.color.neutral.bg.default};
-
-  color: ${theme.color.accent.text.strong};
-  ${theme.font.button.weak}
-`;
-
-export {
-  IconButton,
-  ResumeUploadContainer,
-  ResumeUploadFormContainer,
-  ResumeUploadForm,
-  FieldContainer,
-  Description,
-  MainDescription,
-  SubDescription,
-  Field,
-  Label,
-  FileLabel,
-};
+export { IconButton, ResumeUploadContainer, Description, MainDescription, SubDescription };
