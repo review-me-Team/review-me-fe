dad8878194b45b40626ef2e61a8701e0f05567fd
aken-you <kinsk2839@naver.com>
2024-02-29 14:04:36 +0900

    feat: 이력서 수정 페이지 구현 (#70)

---
+++ src/pages/ResumeUpdate/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Icon } from 'review-me-design-system';
+import ResumeUpdateForm from '@components/ResumeForm/ResumeUpdateForm';
+import { useResumeDetail, useUpdateResume } from '@apis/resumeApi';
+import { Occupation, Scope, useOccupationList, useScopeList } from '@apis/utilApi';
+import { PageMain } from '@styles/common';
+import { ResumeUploadContainer, IconButton, Description, MainDescription } from './style';
+
+interface Location {
+  state: { id: number; title: string; year: number; occupation: string; scope: string };
+}
+
+const ResumeUpdate = () => {
+  const {
+    state: { id: resumeId, title: prevTitle, year: prevYear, occupation: prevOccupation, scope: prevScope },
+  } = useLocation() as Location;
+
+  const navigate = useNavigate();
+
+  const { data: resumeDetail } = useResumeDetail(resumeId);
+  const { data: occupationList } = useOccupationList();
+  const { data: scopeList } = useScopeList();
+
+  const file = resumeDetail?.resumeUrl;
+
+  const prevOccupationState = occupationList?.find(({ occupation }) => occupation === prevOccupation);
+  const prevScopeState = scopeList?.find(({ scope }) => scope === prevScope);
+
+  const [title, setTitle] = useState<string>(prevTitle);
+  const [selectedOccupation, setSelectedOccupation] = useState<Occupation | undefined>(prevOccupationState);
+  const [selectedScope, setSelectedScope] = useState<Scope | undefined>(prevScopeState);
+  const [year, setYear] = useState<number | undefined>(prevYear);
+
+  const { mutate: updateResume } = useUpdateResume();
+
+  return (
+    <PageMain>
+      <IconButton onClick={() => navigate(-1)}>
+        <Icon iconName="leftArrow" />
+        <span>뒤로</span>
+      </IconButton>
+
+      <ResumeUploadContainer>
+        <Description>
+          <MainDescription>이력서 수정</MainDescription>
+        </Description>
+
+        <ResumeUpdateForm
+          resumeId={resumeId}
+          file={file}
+          title={title}
+          year={year}
+          selectedOccupation={selectedOccupation}
+          selectedScope={selectedScope}
+          onChangeTitle={setTitle}
+          onChangeYear={setYear}
+          onChangeSelectedOccupation={setSelectedOccupation}
+          onChangeSelectedScope={setSelectedScope}
+          onSubmit={updateResume}
+        />
+      </ResumeUploadContainer>
+    </PageMain>
+  );
+};
+
+export default ResumeUpdate;
