fac170d005373d0f58c3c7d570799cdffec71d24
aken-you <kinsk2839@naver.com>
2024-03-05 16:37:35 +0900

    feat: jwt 발급 API 요청에 에러가 발생했을 경우 logout 호출 (#76)

--- src/pages/SocialLogin/index.tsx
+++ src/pages/SocialLogin/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import useAuth from '@hooks/useAuth';
+import { useUserContext } from '@contexts/userContext';
 import { ROUTE_PATH } from '@constants';
 
 const SocialLogin = () => {
@@ -8,6 +9,7 @@ const SocialLogin = () => {
   const code = searchParams.get('code');
 
   const { createJwtQuery } = useAuth();
+  const { logout } = useUserContext();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,6 +17,7 @@ const SocialLogin = () => {
       createJwtQuery.mutate(code, {
         onError: () => {
           alert('로그인에 실패했습니다.');
+          logout();
         },
         onSettled: () => {
           navigate(ROUTE_PATH.ROOT);
