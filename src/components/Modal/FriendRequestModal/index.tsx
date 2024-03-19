fb99c5adfaa2d192b2117d76b37fe8df3945a1dc
aken-you <kinsk2839@naver.com>
2024-03-19 20:32:09 +0900

    feat: 친구 추가하는 modal 컴포넌트 생성 (#139)

---
+++ src/components/Modal/FriendRequestModal/index.tsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { Icon, Input, Modal } from 'review-me-design-system';
+import FriendItem from '@components/FriendItem';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
+import useMediaQuery from '@hooks/useMediaQuery';
+import { useUserContext } from '@contexts/userContext';
+import { useUserList } from '@apis/userApi';
+import { UserList, Header, IconButton, SearchUserInstruction } from './style';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const FriendRequestModal = ({ isOpen, onClose }: Props) => {
+  const { jwt } = useUserContext();
+  const { matches: isMDevice } = useMediaQuery({ mediaQueryString: '(max-width: 768px)' });
+
+  const [name, setName] = useState<string>('');
+
+  const { data: userListData, refetch, fetchNextPage } = useUserList({ jwt, start: name });
+  const { setTarget } = useIntersectionObserver({
+    onIntersect: () => {
+      fetchNextPage();
+    },
+    options: { threshold: 0.5 },
+  });
+
+  const userList = userListData?.pages.map((page) => page.users).flat();
+
+  useEffect(() => {
+    if (name.length === 0) return;
+
+    const timer = setTimeout(() => {
+      refetch();
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [name]);
+
+  return (
+    <Modal
+      modalRootId="modal-root"
+      isOpen={isOpen}
+      onClose={() => {
+        onClose();
+      }}
+      width={isMDevice ? '80%' : '37.5rem'}
+    >
+      <Header>
+        <Modal.Title>친구 추가하기</Modal.Title>
+        <IconButton onClick={onClose}>
+          <Icon iconName="xMark" />
+        </IconButton>
+      </Header>
+
+      <Input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+
+      {name.length === 0 && <SearchUserInstruction>사용자 이름을 검색해주세요.</SearchUserInstruction>}
+      {name.length > 0 && userList && userList.length > 0 && (
+        <UserList>
+          {userList.map((user) => (
+            <FriendItem
+              key={user.id}
+              type="none"
+              userId={user.id}
+              userImg={user.profileUrl}
+              userName={user.name}
+            />
+          ))}
+          <div ref={setTarget}></div>
+        </UserList>
+      )}
+      {name.length > 0 && userList && userList.length === 0 && (
+        <SearchUserInstruction>검색어와 일치하는 사용자가 없습니다.</SearchUserInstruction>
+      )}
+    </Modal>
+  );
+};
+
+export default FriendRequestModal;
