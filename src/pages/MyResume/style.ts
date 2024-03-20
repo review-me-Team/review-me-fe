3c30361076862c8c018b17f9789b3893959b9d80
aken-you <kinsk2839@naver.com>
2024-03-20 16:41:03 +0900

    refactor: 내 이력서 페이지에 breakPoints 적용 (#140)

--- src/pages/MyResume/style.ts
+++ src/pages/MyResume/style.ts
@@ -1,36 +1,27 @@
 import { theme } from 'review-me-design-system';
 import styled from 'styled-components';
+import { breakPoints } from '@styles/common';
 
 const MyResumeLayout = styled.div`
   background-color: ${theme.color.neutral.bg.light};
 `;
 
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  gap: 1.5rem;
-  padding: 2rem;
-  min-height: calc(100vh - 4rem);
-
-  @media screen and (max-width: 500px) {
-    padding: 0.625rem;
-  }
-`;
-
 const MyResumeList = styled.ul`
   display: grid;
-  grid-template-columns: repeat(4, minmax(0, 1fr));
-  gap: 1.5rem;
   width: 100%;
 
-  @media screen and (max-width: 1280px) {
+  @media ${breakPoints.desktop} {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    gap: 1.5rem;
+  }
+  @media ${breakPoints.tablet} {
     grid-template-columns: repeat(3, minmax(0, 1fr));
     gap: 1.5rem;
   }
-  @media screen and (max-width: 768px) {
+  @media ${breakPoints.mobile} {
     grid-template-columns: repeat(2, minmax(0, 1fr));
     gap: 1rem 0.625rem;
   }
 `;
 
-export { MyResumeLayout, Main, MyResumeList };
+export { MyResumeLayout, MyResumeList };
