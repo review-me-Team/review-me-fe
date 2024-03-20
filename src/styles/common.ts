17efd2752fd1584006759d166ca6a302b367ef10
aken-you <kinsk2839@naver.com>
2024-03-20 15:56:24 +0900

    feat: PageMain 컴포넌트에 breakPoints 적용 (#140)

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -9,23 +9,25 @@ export const ellipsisStyles = css`
 `;
 
 export const breakPoints = {
-  mobile: `(max-width: 600px)`,
-  tablet: `(max-width: 1119px)`,
-  desktop: `(min-width: 1200px)`,
+  mobile: 'screen and (max-width: 600px)',
+  tablet: 'screen and (max-width: 1119px)',
+  desktop: 'screen and (min-width: 1200px)',
 };
 
 export const PageMain = styled.main<{ $css?: CSSProp }>`
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
-  width: 80%;
-  margin: 2rem auto;
 
-  @media screen and (max-width: 1280px) {
+  @media ${breakPoints.desktop} {
+    width: 80%;
+    margin: 2rem auto;
+  }
+  @media ${breakPoints.tablet} {
     width: 80%;
     margin: 1rem auto;
   }
-  @media screen and (max-width: 768px) {
+  @media ${breakPoints.mobile} {
     width: 90%;
     margin: 0.5rem auto;
   }
