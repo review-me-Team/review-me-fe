5da4f49b3aa159a6a0b549c632e9d8c9e1654b58
aken-you <kinsk2839@naver.com>
2024-03-20 16:24:06 +0900

    refactor: breakPoints에서 tablet 값 수정 (#140)

--- src/styles/common.ts
+++ src/styles/common.ts
@@ -10,7 +10,7 @@ export const ellipsisStyles = css`
 
 export const breakPoints = {
   mobile: 'screen and (max-width: 600px)',
-  tablet: 'screen and (max-width: 1119px)',
+  tablet: 'screen and (max-width: 1199px)',
   desktop: 'screen and (min-width: 1200px)',
 };
 
