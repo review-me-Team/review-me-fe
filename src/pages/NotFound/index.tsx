1224a1e57d496fc29160f19a72769cb9cd62ea43
aken-you <kinsk2839@naver.com>
2024-03-22 15:46:18 +0900

    refactor: NotFound에서 불필요한 태그 제거 (#171)

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -8,11 +8,9 @@ const NotFound = () => {
       <NotFoundSection>
         <Title>페이지를 찾을 수 없습니다.</Title>
         <Description>올바른 주소를 입력했는지 다시 확인해주세요.</Description>
-        <div style={{ display: 'flex' }}>
-          <Button variant="default" size="l">
-            홈으로 이동
-          </Button>
-        </div>
+        <Button variant="default" size="l">
+          홈으로 이동
+        </Button>
       </NotFoundSection>
     </NotFoundLayout>
   );
