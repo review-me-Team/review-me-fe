748decb825f2e56acee1a35f48a1cc0c455b7b86
aken-you <kinsk2839@naver.com>
2024-03-22 09:51:36 +0900

    feat: apiClient 객체 생성 (#169)

---
+++ src/apis/apiClient.ts
@@ -0,0 +1,24 @@
+import { ApiResponse } from './response.types';
+
+const fetcher = async <DataType>(url: string, method: string, options: RequestInit = {}) => {
+  const response = await fetch(url, {
+    method,
+    ...options,
+  });
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { data }: ApiResponse<DataType> = await response.json();
+
+  return data;
+};
+
+export const apiClient = {
+  get: <DataType>(url: string, options: RequestInit = {}) => fetcher<DataType>(url, 'GET', options),
+  post: <DataType>(url: string, options: RequestInit = {}) => fetcher<DataType>(url, 'POST', options),
+  delete: <DataType>(url: string, options: RequestInit = {}) => fetcher<DataType>(url, 'DELETE', options),
+  put: <DataType>(url: string, options: RequestInit = {}) => fetcher<DataType>(url, 'PUT', options),
+  patch: <DataType>(url: string, options: RequestInit = {}) => fetcher<DataType>(url, 'PATCH', options),
+};
