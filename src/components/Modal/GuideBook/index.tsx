cfa87617e7f9fd52f481f67d51edca414b261b04
aken-you <kinsk2839@naver.com>
2024-04-01 17:27:43 +0900

    feat: GuideBook 컴포넌트 생성 (#195)

---
+++ src/components/Modal/GuideBook/index.tsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import feedbackCheckVideo from '@assets/feedback-check.mp4';
+import feedbackListVideo from '@assets/feedback-list.mp4';
+import questionBookMarkVideo from '@assets/question-bookmark.mp4';
+import questionCheckVideo from '@assets/question-check.mp4';
+import questionListVideo from '@assets/question-list.mp4';
+import { Button, Icon, Modal } from 'review-me-design-system';
+import useMediaQuery from '@hooks/useMediaQuery';
+import { breakPoints } from '@styles/common';
+import { IconButton } from '@styles/iconButton';
+import { ButtonContainer, Description, GuideSection, SubDescription } from './style';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const GuideBook = ({ isOpen, onClose }: Props) => {
+  const INIT_GUIDE = 0;
+  const MIN_GUIDE = 1;
+  const MAX_GUIDE = 5;
+  const [currentGuide, setCurrentGuide] = useState<number>(INIT_GUIDE);
+
+  const { matches: isMobile } = useMediaQuery({ mediaQueryString: breakPoints.mobile });
+
+  const handleNextGuide = () => {
+    if (currentGuide < MAX_GUIDE) setCurrentGuide(currentGuide + 1);
+  };
+
+  const handlePrevGuide = () => {
+    if (currentGuide > MIN_GUIDE) setCurrentGuide(currentGuide - 1);
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      modalRootId="modal-root"
+      width={isMobile ? '90%' : 'fit-content'}
+    >
+      {currentGuide === 0 && (
+        <>
+          <Description>
+            <Modal.Title>Review Me를 처음 이용하시나요?</Modal.Title>
+            <Modal.Description>Review Me 사용법을 알려드립니다!</Modal.Description>
+          </Description>
+          <ButtonContainer>
+            <Button variant="outline" size="s" onClick={onClose}>
+              skip
+            </Button>
+            <Button variant="default" size="s" onClick={handleNextGuide}>
+              시작하기
+            </Button>
+          </ButtonContainer>
+        </>
+      )}
+      {currentGuide > 0 && (
+        <GuideSection>
+          {currentGuide === MIN_GUIDE && (
+            <div>
+              <Description>
+                <Modal.Title>피드백</Modal.Title>
+                <Modal.Description>현재 이력서 페이지에 대한 피드백 목록이 표시됩니다.</Modal.Description>
+                <SubDescription>
+                  ex&#41; 현재 이력서가 1페이지라면, 1페이지에 달린 피드백이 보입니다.
+                </SubDescription>
+              </Description>
+              <video
+                src={feedbackListVideo}
+                itemType="video/mp4"
+                width={'100%'}
+                height={'100%'}
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
+          )}
+          {currentGuide === 2 && (
+            <div>
+              <Description>
+                <Modal.Title>피드백 check</Modal.Title>
+                <Modal.Description>해결된 피드백이라면 check 아이콘을 눌러주세요.</Modal.Description>
+              </Description>
+              <video
+                src={feedbackCheckVideo}
+                itemType="video/mp4"
+                width={'100%'}
+                height={'100%'}
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
+          )}
+          {currentGuide === 3 && (
+            <div>
+              <Description>
+                <Modal.Title>예상질문</Modal.Title>
+                <Modal.Description>현재 이력서 페이지에 대한 피드백 목록이 표시됩니다.</Modal.Description>
+                <SubDescription>
+                  ex&#41; 현재 이력서가 1페이지라면, 1페이지에 달린 예상질문이 보입니다.
+                </SubDescription>
+              </Description>
+              <video
+                src={questionListVideo}
+                itemType="video/mp4"
+                width={'100%'}
+                height={'100%'}
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
+          )}
+          {currentGuide === 4 && (
+            <div>
+              <Description>
+                <Modal.Title>예상질문 check</Modal.Title>
+                <Modal.Description>해결된 예상질문이라면 check 아이콘을 눌러주세요.</Modal.Description>
+              </Description>
+              <video
+                src={questionCheckVideo}
+                itemType="video/mp4"
+                width={'100%'}
+                height={'100%'}
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
+          )}
+          {currentGuide === MAX_GUIDE && (
+            <div>
+              <Description>
+                <Modal.Title>예상질문 bookmark</Modal.Title>
+                <Modal.Description>기억하고 싶은 예상 질문은 bookmark 아이콘을 눌러주세요.</Modal.Description>
+              </Description>
+              <video
+                src={questionBookMarkVideo}
+                itemType="video/mp4"
+                width={'100%'}
+                height={'100%'}
+                autoPlay
+                muted
+                loop
+                playsInline
+              />
+            </div>
+          )}
+          <ButtonContainer>
+            <IconButton onClick={handlePrevGuide} disabled={currentGuide === MIN_GUIDE}>
+              <Icon iconName="leftArrow" width={24} height={24} />
+            </IconButton>
+            <Button variant="default" size="s" onClick={onClose}>
+              닫기
+            </Button>
+            <IconButton onClick={handleNextGuide} disabled={currentGuide === MAX_GUIDE}>
+              <Icon iconName="rightArrow" width={24} height={24} />
+            </IconButton>
+          </ButtonContainer>
+        </GuideSection>
+      )}
+    </Modal>
+  );
+};
+
+export default GuideBook;
