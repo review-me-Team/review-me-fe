a5792df887f799ac12c1cbaf83c1cbb329e3f037
aken-you <kinsk2839@naver.com>
2024-04-28 15:32:57 +0900

    fix: 피드백 카테고리 버튼의 type을 button으로 설정 (#228)

--- src/components/FeedbackForm/FeedbackEditForm/index.tsx
+++ src/components/FeedbackForm/FeedbackEditForm/index.tsx
@@ -79,6 +79,7 @@ const FeedbackEditForm = ({
         {labelList?.map(({ id, label }) => {
           return (
             <Label
+              type="button"
               key={id}
               isActive={labelId === id}
               py="0.25rem"
