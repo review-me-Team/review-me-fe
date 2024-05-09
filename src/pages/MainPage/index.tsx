eec35ef7b0ea7af613e26d12e1b328d5d43a912e
kinsk2839 <96980857+aken-you@users.noreply.github.com>
2024-05-09 16:20:48 +0900

    [FE] 메인페이지에서 사용하는 이미지들의 사이즈 스타일 변경 (#237)
    * feat: 반응형 디자인을 위한 breakPoint 상수화 (#230)
    
    * refactor: 메인 페이지에서 가장 먼저 보이는 이미지의 사이즈 변경 (#230)
    
    - 최대 가로 길이를 700px으로 제한
    
    * refactor: 메인 페이지의 피드백, 예상질문, 댓글 이미지의 사이즈 스타일 변경 (#230)
    
    * refactor: 메인 이미지와 설명 문구 사이의 간격을 늘리도록 스타일 수정 (#230)

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -10,8 +10,19 @@ import mainLargeWebp from '@assets/main_1400w.webp';
 import questionJpg from '@assets/question.jpg';
 import questionWebp from '@assets/question_1000w.webp';
 import { Button } from 'review-me-design-system';
+import { screenSize } from '@styles/common';
 import { ROUTE_PATH } from '@constants';
-import { Description, DescriptionText, Guide, Img, Main, MainPageLayout, ReviewMe, Title } from './style';
+import {
+  Description,
+  DescriptionText,
+  Guide,
+  DescriptionImg,
+  Main,
+  MainImg,
+  MainPageLayout,
+  ReviewMe,
+  Title,
+} from './style';
 
 interface Description {
   id: string;
@@ -47,8 +58,6 @@ const description: Description[] = [
 
 const MainPage = () => {
   const navigate = useNavigate();
-  const mainImgBreakPoint = '(max-width: 768px)';
-  const descriptionImgSizes = '(max-width: 600px) 90vw, (max-width: 768px) 300px, 416px';
 
   return (
     <MainPageLayout>
@@ -61,10 +70,15 @@ const MainPage = () => {
         <picture>
           <source
             type="image/webp"
-            srcSet={`${mainSmallWebp} 1040w, ${mainLargeWebp} 1680w`}
-            sizes={`${mainImgBreakPoint} 90vw, 700px`}
+            media={`(max-width: ${screenSize.smallTablet}px)`}
+            srcSet={mainSmallWebp}
           />
-          <Img src={mainJpg} alt="main" />
+          <source
+            type="image/webp"
+            media={`(min-width: ${screenSize.smallTablet + 1}px)`}
+            srcSet={mainLargeWebp}
+          />
+          <MainImg src={mainJpg} alt="main" />
         </picture>
       </Main>
 
@@ -82,15 +96,15 @@ const MainPage = () => {
                 <DescriptionText>{description}</DescriptionText>
               </div>
               <picture>
-                <source type="image/webp" srcSet={imgSrcSet} sizes={descriptionImgSizes} />
-                <Img src={imgSrc} alt={id} />
+                <source type="image/webp" srcSet={imgSrcSet} />
+                <DescriptionImg src={imgSrc} alt={id} />
               </picture>
             </>
           ) : (
             <>
               <picture>
-                <source type="image/webp" srcSet={imgSrcSet} sizes={descriptionImgSizes} />
-                <Img src={imgSrc} alt={id} />
+                <source type="image/webp" srcSet={imgSrcSet} />
+                <DescriptionImg src={imgSrc} alt={id} />
               </picture>
               <div>
                 <Title>{title}</Title>
