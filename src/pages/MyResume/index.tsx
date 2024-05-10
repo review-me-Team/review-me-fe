eeac299e346de37542bdb9ab985d9a69b6f60001
kinsk2839 <96980857+aken-you@users.noreply.github.com>
2024-05-10 15:34:03 +0900

    feat: 내 이력서가 없을 경우, 안내 문구 표시 (#242) (#243)

--- src/pages/MyResume/index.tsx
+++ src/pages/MyResume/index.tsx
@@ -8,7 +8,7 @@ import { useUserContext } from '@contexts/userContext';
 import { useMyResumeList } from '@apis/resumeApi';
 import { PageMain } from '@styles/common';
 import { ROUTE_PATH } from '@constants';
-import { MyResumeList } from './style';
+import { EmptyMyResumeListNotification, MyResumeList } from './style';
 
 const MyResume = () => {
   const navigate = useNavigate();
@@ -28,15 +28,22 @@ const MyResume = () => {
         이력서 pdf 올리기
       </Button>
 
-      <MyResumeList>
-        {myResumeList?.map((resume) => {
-          return (
-            <li key={resume.id}>
-              <MyResumeItem {...resume} />
-            </li>
-          );
-        })}
-      </MyResumeList>
+      {myResumeList && myResumeList.length > 0 ? (
+        <MyResumeList>
+          {myResumeList.map((resume) => {
+            return (
+              <li key={resume.id}>
+                <MyResumeItem {...resume} />
+              </li>
+            );
+          })}
+        </MyResumeList>
+      ) : (
+        <EmptyMyResumeListNotification>
+          <span>작성된 이력서가 없습니다.</span>
+          <span>이력서를 공유하여 다양한 의견과 조언을 받아보세요!</span>
+        </EmptyMyResumeListNotification>
+      )}
       {hasNextPage && !isFetchingNextPage && <div ref={setTarget}></div>}
       {isFetchingNextPage && <Spinner size="4rem" />}
     </PageMain>
