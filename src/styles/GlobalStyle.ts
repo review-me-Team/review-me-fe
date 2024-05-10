97f0650a3d93d977aabf0ef312a6fd8a46140397
aken-you <kinsk2839@naver.com>
2024-05-10 16:55:51 +0900

    feat: 화면 가로 길이가 768px보다 이하일 때 html의 font-size 설정 (#244)

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,7 +1,13 @@
 import { theme } from 'review-me-design-system';
 import { createGlobalStyle } from 'styled-components';
+import { breakPoints } from './common';
 
 export const GlobalStyle = createGlobalStyle`
+  @media ${breakPoints.smallTablet} {
+    html {
+      font-size: 87.5%;
+    }
+  }
   body {
     overflow: auto;
     min-height: 100%;
