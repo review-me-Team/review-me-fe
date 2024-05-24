a581e785ce118aee44b5310f218827f85c9a211a
aken-you <kinsk2839@naver.com>
2024-05-24 17:15:20 +0900

    refactor: pdf viewer의 scale 최솟값 변경 (#256)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,7 +26,7 @@ export const REQUEST_URL = {
 
 export const PDF_VIEWER_SCALE = {
   MAX_SCALE: 2,
-  MIN_SCALE: 0.6,
+  MIN_SCALE: 0.4,
   SCALE_STEP: 0.2,
 } as const;
 
@@ -45,7 +45,7 @@ export const API_CUSTOM_ERROR_CODE = {
 export const FAILURE_MESSAGE = {
   RESUME: {
     NOT_SELECTED_FILE: '파일을 선택해주세요.',
-    LIMITED_FILE_NAME: `파일 이름은 ${RESUME_FILE_NAME_LIMIT}자 이하로 입력해주세요.`,
+    LIMITED_FILE_NAME: `이름이 ${RESUME_FILE_NAME_LIMIT}자 이하인 파일만 가능합니다.`,
     EMPTY_TITLE: '제목을 입력해주세요.',
     NOT_SELECTED_OCCUPATION: '직군을 선택해주세요.',
     NOT_SELECTED_SCOPE: '공개 범위를 선택해주세요.',
