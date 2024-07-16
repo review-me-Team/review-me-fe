1844acab4f1e8c9745bb0fccf6075db832cc9022
aken-you <kinsk2839@naver.com>
2024-07-16 23:47:25 +0900

    feat: 모달이 화면에 띄워져 있는 경우, esc 키를 누르면 닫히는 기능 (#268)
    - useKeyPress 이용

--- src/hooks/useModals.ts
+++ src/hooks/useModals.ts
@@ -1,9 +1,10 @@
 import { ComponentProps, FunctionComponent } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useModalContext } from '@contexts/modalContext';
+import useKeyPress from './useKeyPress';
 
 const useModals = () => {
-  const { push, pop } = useModalContext();
+  const { push, pop, modals } = useModalContext();
 
   const open = (Component: FunctionComponent<any>, props: ComponentProps<FunctionComponent<any>> = {}) => {
     const id = uuidv4();
@@ -15,6 +16,17 @@ const useModals = () => {
   const close = (id: string) => {
     pop(id);
   };
+
+  const closeLast = () => {
+    if (modals.length === 0) return;
+
+    pop(modals[modals.length - 1].id);
+  };
+
+  useKeyPress({
+    targetKey: 'Escape',
+    onKeyPress: closeLast,
+  });
 
   return { open, close };
 };
